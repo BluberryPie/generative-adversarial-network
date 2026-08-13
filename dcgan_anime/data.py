86fd6ef3337299910d8ee402a7696da5b34c4ef0
BluberryPie <blub3rrypie@gmail.com>
2026-08-14 08:02:54 +0900

    add: trasnforms & loader

--- dcgan_anime/data.py
+++ dcgan_anime/data.py
@@ -2,7 +2,17 @@ from pathlib import Path
 from typing import Callable
 
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+
+anime_transforms = transforms.Compose(
+    [
+        transforms.Resize(size=(64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+    ]
+)
 
 
 class AnimeFaceDataset(Dataset):
@@ -18,3 +28,11 @@ class AnimeFaceDataset(Dataset):
         if self.transform is not None:
             image = self.transform(image)
         return image
+
+
+def load_anime(root_dir: Path, batch_size: int) -> DataLoader:
+    anime_dataset = AnimeFaceDataset(root_dir=root_dir, transform=anime_transforms)
+    anime_loader = DataLoader(
+        dataset=anime_dataset, batch_size=batch_size, shuffle=True, drop_last=True
+    )
+    return anime_loader
